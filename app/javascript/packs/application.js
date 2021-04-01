7525c89cc85990fe7dd21f002b2d587ddb3880dd
Jack Reed <phillipjreed@gmail.com>
2021-04-01 14:57:44 -0600

    Add basic react-rails setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,7 @@ $(document).on('ready turbolinks:load', function(event) {
     });
   }
 });
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
