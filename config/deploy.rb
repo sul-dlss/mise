dd0ac7a6def753ee175d6dc88f25d8359c20dcba
Chris Beer <chris@cbeer.info>
2021-04-14 07:48:56 -0700

    Deploy tagged releases to -prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'mise'
 set :repo_url, 'https://github.com/sul-dlss/mise.git'
 set :user, 'mise'
 
-set :branch, 'main' if ENV['DEPLOY']
+set :branch, ENV["REVISION"] || 'main' if ENV['DEPLOY']
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call unless ENV['DEPLOY']
 
 # Default deploy_to directory is /var/www/my_app_name
