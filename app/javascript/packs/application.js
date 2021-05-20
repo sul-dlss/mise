cd0c43f7772dc42157c9e68a6551937a0d268b83
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-05-19 17:35:46 -0700

    Implement modals for duplicate and move workspace
    Fixes #135
    
    Includes:
    * Set up global workspace modal in the application layout (to avoid duplicated bootstrap modal HTML all over the place)
    * Style the workspace modals identically
    * Use StimulusJS to copy the template holding workspace-specific form and accompanying modal guts to the global workspace modal
    * Re-use workspace modal partials across both the workspace/show and workspace card use cases
    * Add a workspaces create route outside the project context to allow for easier duplication of workspaces (let cancancan do its thing)
    * Align the move and duplicate modals with the design
      * Use a dropdown instead of radio buttons, and add the requested text and labels, doing so for both duplicate and move use cases.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,5 @@ const componentRequireContext = require.context('components', true);
 const ReactRailsUJS = require('react_ujs');
 
 ReactRailsUJS.useContext(componentRequireContext);
+
+import "controllers"
